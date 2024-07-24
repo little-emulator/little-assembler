150c31713a3b257bf410c4b7913271b62fed8a14
Nicola Belluti <nicolabelluti@protonmail.com>
2024-07-24 15:26:35 +0200

    Added the base for the LC2 assembler

---
+++ assemblers/src/lc2/mod.rs
@@ -0,0 +1,33 @@
+use crate::ParseError;
+use std::collections::HashMap;
+
+#[derive(Debug, derive_builder::Builder)]
+#[allow(clippy::module_name_repetitions)]
+#[allow(clippy::struct_excessive_bools)]
+pub struct Lc2Assembler {
+    #[builder(default = "false")]
+    optional_end: bool,
+
+    #[builder(default = "true")]
+    nothing_after_end: bool,
+
+    #[builder(default = "false")]
+    optional_orig: bool,
+
+    #[builder(default = "false")]
+    multiple_origs: bool,
+
+    #[builder(default = "false")]
+    enable_stringzp: bool,
+}
+
+impl crate::Assembler for Lc2Assembler {
+    type Address = u16;
+
+    fn assemble(
+        &self,
+        _assembly: &str,
+    ) -> Result<(Vec<u8>, HashMap<String, Self::Address>), ParseError> {
+        todo!();
+    }
+}
--- assemblers/src/lib.rs
+++ assemblers/src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod lc2;
+
+use std::collections::HashMap;
+
+#[allow(clippy::type_complexity)]
+pub trait Assembler {
+    type Address;
+
+    /// # Errors
+    ///
+    /// This method errors if the provided assembly is not valid
+    fn assemble(
+        &self,
+        assembly: &str,
+    ) -> Result<(Vec<u8>, HashMap<String, Self::Address>), ParseError>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(thiserror::Error, Debug)]
+pub enum ParseError {
+    #[error("Token is not valid")]
+    NonValidToken,
+    #[error("I was expecting more Tokens")]
+    NoMoreTokens,
+    #[error("I was not expecting that Token")]
+    UnexpectedToken,
+    #[error("The assembly should start with an \".orig\" directive")]
+    NoOrig,
+    #[error("The assembly should end with an \".end\" directive")]
+    NoEnd,
+    #[error("There should be only an \".orig\" directive")]
+    TooManyOrig,
+    #[error("The \".end\" should be the last directive")]
+    EndNotLast,
+    #[error("The Number Literal is too big")]
+    NumberLiteralTooBig,
+    #[error("The label was declarated more than once")]
+    LabelRedeclaration,
 }
